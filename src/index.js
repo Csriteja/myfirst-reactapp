0d1fae648c52ad1a9a57a659f8b5eec416ed0acd
Csriteja <csriteja.1110@gmail.com>
2019-02-09 23:58:02 +0530

    Files created for individual components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-
+//Need to separate the classes of individual components
 class Square extends React.Component {
   constructor(props) {
     super(props);
